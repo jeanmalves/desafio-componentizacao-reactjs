8f7990d93de50f2ce935b012f5578c1c9824e9a6
Jeam Alves <jeanmalves@gmail.com>
2022-05-25 08:57:31 -0400

    Added movies list component by genre

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,29 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+interface GenreProps {
+  title: string;
+}
+
+interface MoviesByGenre {
+  movies: MovieProps[];
+  selectedGenre: GenreProps;
+}
+
+export function Content(props: MoviesByGenre) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
